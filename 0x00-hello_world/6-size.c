905f039e312132b4d70684913b176b9b34ad8969
ShanzaOG <allanshanza@gmail.com>
2023-02-16 11:41:51 -0800

    size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,6 +7,7 @@
 int main(void)
 {
 	long long int a;
+
 	printf("Size of a char: %d byte(s)\n", (unsigned long)sizeof(char));
 	printf("Size of an int: %d\ byte(s)\n", (unsigned long)sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", (unsigned long)sizeof(long int));
