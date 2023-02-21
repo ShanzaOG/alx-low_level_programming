7ec543f32cfb5f64326feff954a6b9cf92e2db57
ShanzaOG <allanshanza@gmail.com>
2023-02-21 13:49:37 -0800

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - computes absolute value
+ * @r:integer value argument
+ * Description: Function returns the absolute value
+ * Return: integer
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}
+
