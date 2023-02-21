32f8160d22144094fe1cf86aa4436b715c5a2ca4
ShanzaOG <allanshanza@gmail.com>
2023-02-21 07:20:27 -0800

    batty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,7 +8,7 @@ void print_alphabet(void)
 {
 	char str;
 
-        for (str = 'a'; str <= 'z'; str++)
-                _putchar(str);
-        _putchar('\n');
+	for (str = 'a'; str <= 'z'; str++)
+		_putchar(str);
+	_putchar('\n');
 }
