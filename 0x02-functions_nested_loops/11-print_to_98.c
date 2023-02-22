c2d4fc1579fb26973598767c713c84bb0d616780
ShanzaOG <allanshanza@gmail.com>
2023-02-21 16:20:31 -0800

    natural numbers to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_to_98 - prints natural numbers
+ * @n: takes an integer n
+ * Description: prints all natural numbers using _putchar
+ * Return: nothing
+ */
+void print_to_98(int n)
+{
+	int i = 0;
+
+	if (n > 98)
+	{
+		for (i = 98; i >= n; i--)
+			_putchar(i);
+	}
+	else if (n < 0)
+	{
+		i = n;
+		while (i <= 98)
+			_putchar(i);
+	}
+	else
+		for (i = 0; i <= 98; i++)
+			_putchar(i);
+}
