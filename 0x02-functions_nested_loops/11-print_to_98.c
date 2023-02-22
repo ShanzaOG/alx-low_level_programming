e71d01ebca935028b134c7363b96178e80d8826c
ShanzaOG <allanshanza@gmail.com>
2023-02-21 16:27:28 -0800

    natural numbers to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,18 +9,30 @@ void print_to_98(int n)
 {
 	int i = 0;
 
-	if (n > 98)
+	if (n >= 98)
 	{
 		for (i = 98; i >= n; i--)
+		{
 			_putchar(i);
+			_putchar(',');
+			_putchar(' ');
+		}
 	}
 	else if (n < 0)
 	{
 		i = n;
 		while (i <= 98)
+		{
 			_putchar(i);
+			_putchar(',');
+			_putchar(' ');
+		}
 	}
 	else
 		for (i = 0; i <= 98; i++)
+		{
 			_putchar(i);
+			_putchar(',');
+			_putchar(' ');
+		}
 }
