ad1f7628c3b09f6ae2d1cf9896788a556ff2b57b
ShanzaOG <allanshanza@gmail.com>
2023-02-21 17:16:00 -0800

    n times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * prikt_times_table - prints n times table
+ * @n: integer n
+ * Description: prints using _putchar
+ * Return: nothing
+ */
+void print_times_table(int n)
+{
+	int i, j, k;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			k = i * j;
+
+			if ((k / 10) == 0)
+			{
+				if (j != 0)
+					_putchar(' ');
+				_putchar(k + '0');
+
+				if (j == n)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+				if (j == n)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
