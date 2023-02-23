cbb74022ebba0b87763e4e8d79afc87cd2ef3d37
ShanzaOG <allanshanza@gmail.com>
2023-02-23 07:52:26 -0800

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_diagonal - draws a straight line
+ * @n: integer n
+ * Description: uses _putchar to print line
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+	int j, i;
+
+	if (n > 0)
+	{
+		for (j = 0; j < n; j++)
+		{
+			for (i = 0; i < j; i++)
+					_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
