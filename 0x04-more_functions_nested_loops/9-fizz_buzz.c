6d5ef35a86ed1ed0f810a2b5ccdd081ef2eced92
ShanzaOG <allanshanza@gmail.com>
2023-02-23 10:32:39 -0800

    Fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,21 +10,21 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 5 == 0 && i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("BuzzFizz ");
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
 		}
-		else if (i % 5 == 0 && i % 3 == 0)
+		else if (i % 3 == 0)
 		{
-			printf("BuzzFizz ");
+			printf("Fizz ");
 		}
 		else
 			printf("%d ", i);
 	}
-	printf("Buzz\n");
+	printf("\n");
 	return (0);
 }
