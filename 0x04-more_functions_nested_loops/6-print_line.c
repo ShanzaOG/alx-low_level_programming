32c66398e4bf91cdf89dce5f0e604d02e289eafc
ShanzaOG <allanshanza@gmail.com>
2023-02-23 06:58:10 -0800

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line
+ * @n: integer n
+ * Description: uses _putchar to print line
+ * Return: nothing
+ */
+void print_line(int n)
+{
+	char i = '_';
+	int j;
+
+	if (n > 0)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
