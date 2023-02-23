34f406d456876176721a73944c8ad545d0516bec
ShanzaOG <allanshanza@gmail.com>
2023-02-23 03:18:30 -0800

    ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more-numbers - prints 10 times
+ * Description: uses _putchar
+ * Return: nothing
+ */
+void more_numbers(void)
+{
+	int i;
+	int j = 0;
+
+	while (j < 10)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if(i > 9)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		j++;
+		_putchar('\n');
+	}
+}
