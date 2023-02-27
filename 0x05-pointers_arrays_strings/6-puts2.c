87e66dac72cc7dfb9549a34ea83a1cb1605365a0
ShanzaOG <allanshanza@gmail.com>
2023-02-27 12:28:57 -0800

    alternate char of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - check description
+ * @str: pointer to char
+ * Description: prints characters of a string followed by a new line
+ * Return: Nothing
+ */
+void puts2(char *str)
+{
+	char *p = str;
+
+	while (*p != '\0')
+	{
+		_putchar(*p);
+		p+=2;
+	}
+	_putchar('\n');
+}
