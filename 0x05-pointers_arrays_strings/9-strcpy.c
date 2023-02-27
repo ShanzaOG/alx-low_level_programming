c00aca16f80cb1ff5091d8faf3d906708c4f1bb8
ShanzaOG <allanshanza@gmail.com>
2023-02-27 15:33:11 -0800

    copying a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strpy - check description
+ * @dest: pointer to a char
+ * @src: pointer to a char
+ * Description: copies string to a buffer
+ * Return: pointer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *p = dest;
+
+	while (*src)
+		*dest++ = *src++;
+	return (p);
+}
