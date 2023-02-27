550abed3625a2b376a97f773ea24b61c006c4898
ShanzaOG <allanshanza@gmail.com>
2023-02-27 04:49:57 -0800

    print reversed string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _puts - check description
+ * @str: pointer to char
+ * Description: prints a string followed by a new line
+ * Return: Nothing
+ */
+int _strlen(char *s)
+{
+        int i = 0;
+
+        while (s[i] != '\0')
+        {
+                i++;
+        }
+        return (i);
+}
+void rev_string(char *s)
+{
+	char p;
+	int i = 0;
+	int len = _strlen(s) - 1;
+
+	while (i < len)
+	{
+		p = s[i];
+		s[i++] = s[len];
+		s[len--] = p;
+	}
+}
