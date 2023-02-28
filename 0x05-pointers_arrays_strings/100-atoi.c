d79cedac504cbbe069e6d862ee7cbafda89e77be
ShanzaOG <allanshanza@gmail.com>
2023-02-28 08:14:39 -0800

    string to int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -22,7 +22,7 @@ int _atoi(char *s)
 	for (; s[i] != '\0'; i++)
 	{
 		if (s[i] >= '0' && s[i] <= '9')
-			result *= 10 +(s[i] - '0');
+			result = result * 10 +(s[i] - '0');
 		else
 			return (0);
 	}
