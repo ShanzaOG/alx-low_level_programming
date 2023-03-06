55c35dc8d384c9d1d76f2f705556c69855977501
ShanzaOG <allanshanza@gmail.com>
2023-03-06 02:28:28 -0800

    removed stdio.h

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _strchr - locates character in string
@@ -21,6 +20,6 @@ char *_strchr(char *s, char c)
 	}
 	if (c == '\0')
 		return (s);
-	return (NULL);
+	return ((void*)0);
 }
 
