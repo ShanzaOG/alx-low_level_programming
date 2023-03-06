b1edbfbc0bfcf5d7f547316f350161ca75300503
ShanzaOG <allanshanza@gmail.com>
2023-03-06 12:43:40 -0800

    chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_chessboard - check description
- * @a[][8]: 2-d array
+ * @a:pointer to  2-d array
  * Description: prints chess board
  * Return: nothing
  */
@@ -9,7 +9,7 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for(i = 0; i < 8; j++)
+	for (i = 0; i < 8; j++)
 	{
 		for (j = 0; j < 8; j++)
 		{
