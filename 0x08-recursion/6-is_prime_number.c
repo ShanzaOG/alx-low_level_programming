5e703d4a6b62cb392e2d39ad9ec651e13dd26a28
ShanzaOG <allanshanza@gmail.com>
2023-03-08 06:01:28 -0800

    prime numbers

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main,h"
+/**
+ * is_prime_number - check description
+ * @n: integer
+ * @i: integer
+ * Description: checks if number is prime
+ * Return: 1 if prime and 0 if not
+ */
+int is_prime_number(int n, int i)
+{
+	if (n <= 2)
+		return ((n == 2) ? 1 : 0);
+	if (n % i == 0)
+		return (0);
+	if (i * i > n)
+		return (1);
+	return (is_prime_number(n, i + 1));
+}
+
+/**
+ * is_prime_number - check description
+ * @n: integer
+ * Description: checks if number is prime
+ * Return: 1 if prime and 0 if not
+ */
+int is_prime_number(int n)
+{
+	return (is_prime_number(n, 2));
+}
