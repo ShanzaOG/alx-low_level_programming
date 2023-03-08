4330584921c3560d1a5440688412425220ac7f38
ShanzaOG <allanshanza@gmail.com>
2023-03-08 03:18:59 -0800

    string length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,7 +7,10 @@
  */
 int _strlen_recursion(char *s)
 {
+	int n = 0;
 	if (*s == '\0')
-		return;
-	return (1 + _strlen_recursion(s + 1));
+		return (0);
+	else
+		n = 1 + _strlen_recursion(s + 1);
+	return (n);
 }
