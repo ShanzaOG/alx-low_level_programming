09175b5945e1fb6497d2a2a89ddcb6d642075ff3
ShanzaOG <allanshanza@gmail.com>
2023-03-20 07:16:58 -0700

    stuct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog - this is a user defined data type called dog
+ * @name: pointer to char
+ * @age: pointer to float
+ * @owner: pointer to char
+ *
+ * Description: takes three attributes name, age, owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
