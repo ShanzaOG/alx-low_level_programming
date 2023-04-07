f2b75f467746cab8281d57e852a579e3f088ba01
ShanzaOG <allanshanza@gmail.com>
2023-04-07 06:43:48 -0700

    decimal to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,15 +7,20 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask;
+	unsigned long int mask = 32768;
+	int printed = 0;
 
-	mask = 1ul << (sizeof(unsigned long int) * 8 - 1);
 	while (mask > 0)
 	{
-		if (n & mask)
+		if ((n & mask) == mask)
+		{
+			printed = 1;
 			_putchar('1');
-		else
+		}
+		else if (printed)
 			_putchar('0');
 		mask >>= 1;
 	}
+	if (!printed)
+		_putchar('0');
 }
