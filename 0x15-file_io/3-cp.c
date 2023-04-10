9060ec2d60d35e6d45fd5d5201e2a7ad88d7346c
ShanzaOG <allanshanza@gmail.com>
2023-04-09 17:05:39 -0700

    Copies the text of one file to another*

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
 	}
 
 	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	file_to = open(argv[2], O_CREAT | O_RDWR | O_TRUNC | O_APPEND, 0664);
 	check_file(file_from, file_to, argv);
 
 	ncharx = 1024;
