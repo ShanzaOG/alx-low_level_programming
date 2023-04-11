57cecf050056c79fcbc42fdf9b838ef3bca472db
ShanzaOG <allanshanza@gmail.com>
2023-04-10 22:37:38 -0700

    corrected

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,20 +7,8 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 32768;
-	int printed = 0;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	while (mask > 0)
-	{
-		if ((n & mask) == mask)
-		{
-			printed = 1;
-			_putchar('1');
-		}
-		else if (printed)
-			_putchar('0');
-		mask >>= 1;
-	}
-	if (!printed)
-		_putchar('0');
+	_putchar((n & 1) + '0');
 }
