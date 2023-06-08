c1d5fa5242da8b2a881b50055af48ae834a67e5b
ShanzaOG <allanshanza@gmail.com>
2023-06-08 12:57:10 -0700

    corections

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,26 +1,27 @@
 #include "hash_tables.h"
+#include <stdlib.h>
 
 /**
- * hash_table_create - Creates a hash table.
- * @size: The size of the array.
- *
- * Return: If an error occurs - NULL.
- *         Otherwise - a pointer to the new hash table.
+ * hash_table_create - A function that creates a hash table.
+ * @size: The size of hash table.
+ * Return: A pointer to the new hash table in memory (heap).
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int a;
-	hash_table_t *hash_t;
+	unsigned long int i = 0;
+	hash_table_t *new_table = NULL;
 
-	hash_t = malloc(sizeof(hash_table_t));
-	if (hash_t == NULL)
+	new_table = malloc(sizeof(hash_table_t));
+	if (!new_table)
 		return (NULL);
-	hash_t->size = size;
-	hash_t->array = malloc(sizeof(list_node *) * size);
-	if (hash_t->array == NULL)
+	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (!new_table->array)
+	{
+		free(new_table);
 		return (NULL);
-	for (a = 0; a < size; a++)
-		hash_t->array[a] = NULL;
-
-	return (hash_t);
+	}
+	for (; i < size; i++)
+		(new_table->array)[i] = NULL;
+	return (new_table);
 }
